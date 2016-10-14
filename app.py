a410695100e9daa5a2bd224e654896cc7473a7c5
Kamal Mustafa <kamal.mustafa@gmail.com>
2016-10-14 09:11:04 +0800

    add licence

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+"""
+Copyright 2016 Kamal Mustafa
+
+This file is part of Dropibit.
+
+Dropibit is free software: you can redistribute it and/or modify
+it under the terms of the Afferor GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Dropibit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the Affero GNU General Public License
+along with Dropibit.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import os
 import sys
 
