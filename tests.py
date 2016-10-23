be1c0438d7250a9ae2f6c72e3a6af5b7dafe7a00
Kamal Mustafa <kamal.mustafa@gmail.com>
2016-10-24 04:05:24 +0800

    initial test upload, still fail

--- tests.py
+++ tests.py
@@ -27,4 +27,53 @@ class ServiceTest(unittest.TestCase):
         assert total_alias == num_alias, (total_alias, num_alias)
         assert file_.name == file_name, (file_.name, file_name)
 
-unittest.main()
+import cherrypy
+from cherrypy.test import helper
+
+admin_email = 'admin@dropibit.com'
+class SimpleCPTest(helper.CPWebCase):
+    def setup_server():
+        db.db.init(':memory:')
+        db.db.create_tables([db.User, db.File, db.FileAlias])
+        user = db.User.create(email=admin_email)
+
+        from app import Root
+        cherrypy.config.update({
+            'database': db,
+            'project_root': config.PROJECT_ROOT,
+            'storage_dir': config.STORAGE_DIR,
+            'admin_email': admin_email,
+        })
+        cherrypy.tree.mount(Root(), '/', config.APP_CONFIG)
+    setup_server = staticmethod(setup_server)
+
+    def setUp(self):
+        self.setup_class()
+
+    def tearDown(self):
+        self.teardown_class()
+
+    def test_index(self):
+        self.getPage("/")
+        self.assertStatus('200 OK')
+        self.assertHeader('Content-Type', 'text/html;charset=utf-8')
+
+    def test_upload(self):
+        # Test upload
+        body = '\r\n'.join([
+            '--x',
+            'Content-Disposition: form-data; name="myfile"; filename="hello.txt"',
+            'Content-Type: text/plain',
+            '',
+            '%s',
+            '--x--'])
+        partlen = 200 - len(body)
+        b = body % ("x" * partlen)
+        h = [("Content-type", "multipart/form-data; boundary=x"),
+             ("Content-Length", "%s" % len(b))]
+        self.getPage("/api/upload/", h, "POST", b)
+        self.assertStatus('200 OK')
+        self.assertHeader('Content-Type', 'application/json')
+
+if __name__ == '__main__':
+    unittest.main()
